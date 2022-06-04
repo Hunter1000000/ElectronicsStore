1c6b3621733192523764c5ee29751d37277fe530
Egor <Egor@LAPTOP-QKCEOORU>
2022-06-04 03:03:14 +0200

    Добавьте файлы проекта.

---
+++ ElectronicsStore/SqlConnect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicsStore
+{
+    class SqlConnect
+    {
+        public double[] connect()
+        {
+            return 1.2;
+        }
+    }
+}
